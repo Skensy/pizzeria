d1465cdd3aabc8ae60335207f60a6d99c994aa9f
Skensy <leonardo.manganelli@icloud.com>
2026-01-14 23:04:40 +0100

    Add initial files for Pizzeria Bella Napoli website
    - Created HTML structure for the main page with navigation and footer.
    - Added CSS and JavaScript files for styling and functionality.
    - Included SVG icons for branding and UI elements.
    - Implemented routing and server-side rendering for dynamic content.
    - Established a responsive layout for better user experience on various devices.

---
+++ out/_next/static/8W5-Ot8UL3Y61nRxonVJf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pizzeria//_next/:path+",
+        "destination": "/pizzeria/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
